d6307523df03f0ef0f10e0992f0c138c10b4179a
yndira <yndinatera10@gmail.com>
2020-10-25 21:15:02 -0300

    first commit

---
+++ src/components/View.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Header from './header/Header';
+
+const View = () => {
+    return (
+        <Fragment>
+            <Header />
+        </Fragment>
+    )
+}
+
+export default View;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './header.css';
+import img1 from '../../img/photo1-mobile.png';
+import img2 from '../../img/photo1-desktop.png'
+
+const Header = () => {
+    return (
+        <header>
+            <div className='top-bar'>
+                <h1> &#60; Naterayc <span>&#47;&#62;</span></h1>
+
+            </div>
+            <div className='info-container'>
+                <div className="info">
+                    <h2>¡Hola, soy Yndira Natera!</h2>
+                    <h3>Desarrolladora Front-end</h3>
+                    <h3>e Ingeniero electricista.</h3>
+                    <button type='button'>Proyectos</button>
+                </div>
+                <picture>
+                    <source srcSet={img2} media='(min-width:600px)'/>
+                    <img src={img1} alt='foto1' />
+                </picture>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
